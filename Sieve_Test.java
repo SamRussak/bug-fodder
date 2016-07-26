384f532d123a2c55e7be7a5ed33a20b4be9473eb
SamRussak <sjr59@pitt.edu>
2016-07-26 13:06:07 -0400

    Deliverable 6

--- Sieve_Test.java
+++ Sieve_Test.java
@@ -8,21 +8,21 @@ public class Sieve_Test {
 
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
-	
+	//Test good values
 	@Test
 	public void GoodArgumentTest() {	
 		String[] arg = {"10"};		
 		Sieve.main(arg);		
 		assertTrue(Sieve._max == 10);
 	}
-	
+	//Test empty array
 	@Test
 	public void ZeroSizeTest() {	
 		String[] arg = {};		
 		Sieve.main(arg);		
 		assertTrue(Sieve._max == 100);	
 	}
-	
+	//test method empty array
 	@Test
 	public void EmptyTestMethod() {	
 		String[] arg = {};
